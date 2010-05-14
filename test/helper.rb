ad837f354274ffc1ff5bfad3ddd0f3515dc6f8b5
Ryan Lewis <c00lryguy@gmail.com>
2010-05-14 15:33:16 -0400

    Adding lib to LOAD PATH in test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'shoulda'
 require 'test/unit'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rulebook'
 
 class Test::Unit::TestCase
