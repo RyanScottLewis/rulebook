2406b53ae6cf67a3652f1c0b64f02aa4b60d8cb9
Ryan Lewis <c00lryguy@gmail.com>
2011-04-30 11:54:49 -0400

    prepare for regexp validation

--- lib/rulebook/rule.rb
+++ lib/rulebook/rule.rb
@@ -5,17 +5,14 @@ class Rulebook
     attr :block
     
     def initialize(what_to_capture, &block)
+      # TODO: Match more than Regexp. Strings and symbols pls.
       raise(TypeError, 'what_to_capture must be of type Regexp') unless what_to_capture.is_a?(Regexp)
       raise(ArgumentError, 'a block is needed') unless block_given?
+
       @what_to_capture, @block = what_to_capture, block
     end
     
-    def [](query)
-      # We have to check for start and end of string regexp..
-      # We also have to check for strings and symbols and all of that jazz
-
-      query.to_s.downcase.match(@what_to_capture)
-    end
+    def [](query); query.to_s.downcase.match(@what_to_capture); end
     alias_method :match_against, :[]
     def matches_against?(query); !self[query].nil?; end
   end
